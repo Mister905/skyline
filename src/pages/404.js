ad5e51a6ea8f46c427bc0fd1376f6c0d9322ae6d
James McCarthy <jamesmccarthyxci@gmail.com>
2020-02-22 15:45:22 -0500

    Custom 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,17 +1,34 @@
-import React from "react"
+import React, { Component } from "react"
+import Pooh from "../assets/img/pooh.jpg"
 import { Link } from "gatsby"
-import Head from "../components/layout/head"
-import Layout from "../components/layout/layout"
+import Head from "../components/Head"
 
 const NotFound = () => {
   return (
-    <Layout>
-      <Head title="404" />
-      <h1>Page Not Found</h1>
-      <p>
-        <Link to="/">Home</Link>
-      </p>
-    </Layout>
+    <div>
+      <Head title="404 Error" />
+
+      <div className="container mt-50">
+        <div className="row">
+          <div className="col m12 center-align mt-50">
+            <div className="component-heading fw-600">Oh bother...</div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col m8 offset-m2">
+            <img src={Pooh} alt="Profile Image" className="responsive-img" />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col m12 center-align">
+            <p className="sorry fw-600">
+              We couldn't find what you're looking for.
+            </p>
+            <p className="sorry fw-600">Sorry about that.</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
