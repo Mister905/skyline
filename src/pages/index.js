f3f99febdea125a049f0da75d563562b7b3822ef
James McCarthy <jamesmccarthyxci@gmail.com>
2020-02-23 17:57:14 -0500

    Test

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
-import Layout from "../components/layout/layout"
+import Layout from "../components/layout/Layout"
 import { Link, graphql, useStaticQuery } from "gatsby"
 import Head from "../components/layout/Head"
 
